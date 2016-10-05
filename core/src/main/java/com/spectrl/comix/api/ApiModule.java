6fa09795f97a15f0201e6fb59a66cc676850ed67
Kavi Dhokia <kavi@spectrl.com>
2016-10-06 00:07:08 +0100

    Modify Cache-Control response headers

--- core/src/main/java/com/spectrl/comix/api/ApiModule.java
+++ core/src/main/java/com/spectrl/comix/api/ApiModule.java
@@ -3,20 +3,24 @@ package com.spectrl.comix.api;
 import com.squareup.moshi.Moshi;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.Cache;
+import okhttp3.CacheControl;
 import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.moshi.MoshiConverterFactory;
 
-import static okhttp3.logging.HttpLoggingInterceptor.*;
+import static okhttp3.logging.HttpLoggingInterceptor.Level;
 
 /**
  * Created by Kavi @ SPECTRL Ltd. on 27/09/2016.
@@ -27,6 +31,7 @@ public class ApiModule {
     private static final String MARVEL_API_URL = "http://gateway.marvel.com/";
     private static final String HTTP_CACHE_DIR = "http-cache";
     private static final int CACHE_SIZE = 10 * 1024 * 1024; // 10 MiB
+    private static final String CACHE_CONTROL = "Cache-Control";
 
     private final boolean isDebug;
 
@@ -52,12 +57,26 @@ public class ApiModule {
     }
 
     @Provides @Singleton
-    OkHttpClient provideOkHttpClient(MarvelAuthorizationInterceptor marvelAuthorizationInterceptor, Cache cache) {
+    Interceptor provideCacheControlInterceptor() {
+        return chain -> {
+            Response originalResponse = chain.proceed(chain.request());
+            CacheControl cacheControl = new CacheControl.Builder()
+                    .maxAge(60, TimeUnit.SECONDS)
+                    .build();
+            return originalResponse.newBuilder()
+                    .header(CACHE_CONTROL, cacheControl.toString())
+                    .build();
+        };
+    }
+
+    @Provides @Singleton
+    OkHttpClient provideOkHttpClient(MarvelAuthorizationInterceptor marvelAuthorizationInterceptor, Interceptor cacheControl, Cache cache) {
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-        logging.setLevel(isDebug ? Level.BASIC : Level.NONE);
+        logging.setLevel(isDebug ? Level.HEADERS : Level.NONE);
         return new OkHttpClient.Builder()
-                .addInterceptor(marvelAuthorizationInterceptor)
                 .addInterceptor(logging)
+                .addInterceptor(marvelAuthorizationInterceptor)
+                .addInterceptor(cacheControl)
                 .cache(cache)
                 .build();
     }
