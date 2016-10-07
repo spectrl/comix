c73dd2a2da20c8484e3a0258e242b63c9fd4ee6c
Kavi Dhokia <kavi@spectrl.com>
2016-10-07 17:14:56 +0100

    BudgetPredicate tests

---
+++ core/src/test/java/com/spectrl/comix/collection/data/repository/ComicStoreTest.java
@@ -0,0 +1,10 @@
+package com.spectrl.comix.collection.data.repository;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kavi @ SPECTRL Ltd. on 07/10/2016.
+ */
+public class ComicStoreTest {
+
+}
